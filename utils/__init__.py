2c533619c252b00a2c7ac48917413ab9cc42cf7c
koer <lego3ilya@gmail.com>
2025-12-02 00:55:18 +0100

    Implemented Imputer class for images

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,14 @@
 from .data_loader import load_mnist, load_fashion_mnist
-from .training_ae import train_autoencoder
-from .training_dec import train_dec
-from .imputer import knn_impute_image, mean_impute_image
+from .imputer import KNNImageImputer, MeanImageImputer
 from .missingness import Missingness
 from .pipeline import run_dec_pipeline
-from .plotting import (plot_dec_performance,
-                       plot_ae_losses,
-                       plot_ae_reconstructions)
+from .plotting import (
+    plot_dec_performance,
+    plot_ae_losses,
+    plot_ae_reconstructions
+)
+from .training_ae import train_autoencoder
+from .training_dec import train_dec
 
 __all__ = [
     'load_mnist',
@@ -15,8 +17,8 @@ __all__ = [
     'train_autoencoder',
     'train_dec',
 
-    'knn_impute_image',
-    'mean_impute_image',
+    'KNNImageImputer',
+    'MeanImageImputer',
     'Missingness',
 
     'run_dec_pipeline',
