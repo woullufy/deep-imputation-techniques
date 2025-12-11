Fixed SklearnGMMImageImputation and added accuracy calculation

utils/imputer.py
@@ -82,8 +82,8 @@ class KNNImageImputer(ImputerStrategy):
         return out_channel
 
 
-class GMMImageImputer(ImputerStrategy):
-    def __init__(self, n_components=10, ink_threshold=0.5):
+class SklearnGMMImageImputer(ImputerStrategy):
+    def __init__(self, n_components=10, ink_threshold=0.1):
         self.n_components = n_components
         self.ink_threshold = ink_threshold
         self.model = None
@@ -133,7 +133,7 @@ class GMMImageImputer(ImputerStrategy):
         density = np.exp(log_density)
 
         log_density_train = self.model.score_samples(X_train)
-        ref_density = np.percentile(np.exp(log_density_train), 80)
+        ref_density = np.percentile(np.exp(log_density_train), 50)
 
         imputed_vals_np = np.clip(density / (ref_density + 1e-10), 0, 1)
 

utils/pipeline.py
@@ -1,4 +1,6 @@
+import numpy as np
 import torch
+from scipy.optimize import linear_sum_assignment
 from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
 from torch import optim
 from torch.nn import KLDivLoss, MSELoss
@@ -97,6 +99,22 @@ def run_dec_pipeline(
 
     ari = adjusted_rand_score(y_true, y_pred)
     nmi = normalized_mutual_info_score(y_true, y_pred)
+    acc = clustering_accuracy(y_true, y_pred)
 
-    print(f'Result: ARI={ari:.4f} | NMI={nmi:.4f}')
-    return ari, nmi
+    print(f'Result: ARI={ari:.4f} | NMI={nmi:.4f} | ACC={acc:.4f}')
+    return ari, nmi, acc
+
+
+def clustering_accuracy(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    D = max(y_pred.max(), y_true.max()) + 1
+    cost_matrix = np.zeros((D, D), dtype=int)
+
+    for i in range(len(y_true)):
+        cost_matrix[y_pred[i], y_true[i]] += 1
+
+    row_ind, col_ind = linear_sum_assignment(cost_matrix.max() - cost_matrix)
+    accuracy = cost_matrix[row_ind, col_ind].sum() / len(y_true)
+    return accuracy